082b0ac9b78d31697c30652c41a1e7d6fca8ce46
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-01-31 20:34:59 -0500

    Making progress

--- C++/Linked List/main.cpp
+++ C++/Linked List/main.cpp
@@ -1,53 +1,173 @@
 #include <iostream>
 
 template <typename T>
-class Node {
-    public:
-        T value;
-        Node* next;
-
-        Node(T value) {
-            this->value = value;
-            this->next = NULL;
-        }
+struct SingleNode {
+    T value;
+    SingleNode* next;
 };
 
 template <typename T>
-class LinkedList {
-    public:
-        Node<T>* head;
-        Node<T>* current;
+struct DoubleNode {
+    T value;
+    DoubleNode* next;
+    DoubleNode* prev;
+};
 
-        LinkedList(T value) {
-            Node<T>* node = new Node<T>(value);
-            this->head = node;
-            this->current = NULL;
+template <typename T>
+class SinglyLinkedList {
+   private:
+    SingleNode<T>* head_ = nullptr;
+    SingleNode<T>* tail_ = nullptr;
+
+   public:
+    SinglyLinkedList(T value) {
+        SingleNode<T>* node = new SingleNode<T>();
+        node->value = value;
+        node->next = nullptr;
+
+        this->head_ = node;
+        this->tail_ = node;
+    }
+
+    ~SinglyLinkedList() {
+        SingleNode<T>* current = this->head_;
+
+        while (current) {
+            SingleNode<T>* temp = current;
+            current = current->next;
+            delete temp;
         }
+    }
 
-        void add(T value) {
-            Node<T>* node = new Node<T>(value);
-            this->current = this->head;
-            while (this->current->next != NULL) {
-                this->current = this->current->next;
-            }
-            this->current->next = node;
+    void Append(T value) {
+        SingleNode<T>* node = new SingleNode<T>();
+        node->value = value;
+        node->next = nullptr;
 
+        if (!this->tail_) {
+            this->head_ = node;
+            this->tail_ = node;
+        } else {
+            this->tail_->next = node;
+            this->tail_ = node;
+        }
+    }
+
+    void Prepend(T value) {
+        SingleNode<T>* node = new SingleNode<T>();
+        node->value = value;
+        node->next = nullptr;
+
+        if (!this->head_) {
+            this->head_ = node;
+            this->tail_ = node;
+        } else {
+            node->next = this->head_;
+            this->head_ = node;
+        }
+    }
+
+    void Insert(int index, T value) {
+        if (index < 0) {
+            throw std::runtime_error(
+                "Index error: Insertion index cannot be less than zero.");
+        }
+        if (index == 0) {
+            Prepend(value);
             return;
         }
 
+        SingleNode<T>* current = this->head_;
+        if (!current) {
+            throw std::runtime_error("Index error: Out of range.");
+        }
+
+        int i = 0;
+        int insert_index = index - 1;
+        while (current) {
+            if (i == insert_index) {
+                SingleNode<T>* node = new SingleNode<T>();
+                node->value = value;
+                node->next = current->next;
+                current->next = node;
+
+                if (node->next == nullptr) {
+                    this->tail_ = node;
+                }
+
+                return;
+            }
+            current = current->next;
+            i++;
+        }
+
+        if (i + 1 == index) {
+            Append(value);
+            return;
+        }
+
+        throw std::runtime_error("Index error: Out of range.");
+    }
+
+    void Clear() {
+        SingleNode<T>* current = this->head_;
+
+        while (current) {
+            SingleNode<T>* temp = current;
+            current = current->next;
+            delete temp;
+        }
+
+        this->head_ = nullptr;
+        this->tail_ = nullptr;
+    }
+    // contains()
+    // []
+    // removeAt()
+    // removeHead()
+    // removeTail()
+    // remove()
+    T Head() {
+        if (!this->head_) {
+            throw std::runtime_error("List is empty.");
+        }
+        return this->head_->value;
+    }
+
+    T Tail() {
+        if (!this->tail_) {
+            throw std::runtime_error("List is empty.");
+        }
+        return this->tail_->value;
+    }
+
+    void PrintAll() {
+        SingleNode<T>* current = this->head_;
+        while (current) {
+            std::cout << current->value << " -> ";
+            current = current->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
 };
 
 int main() {
-    LinkedList list = LinkedList(5);
-    list.add(10);
-    list.add(20);
+    SinglyLinkedList<int> list(10);
+    list.Prepend(5);
+    list.Append(20);
 
-    list.current = list.head;
-    while (list.current->next != NULL) {
-        std::cout << list.current->value << std::endl;
-        list.current = list.current->next;
+    list.Insert(3, 30);  // Inserts `30` at the last valid index (works!)
+    list.PrintAll();     // Expected: 5 -> 10 -> 20 -> 30 -> nullptr
+
+    list.Insert(5, 40);  // Should now APPEND instead of erroring
+    list.PrintAll();     // Expected: 5 -> 10 -> 20 -> 30 -> 40 -> nullptr
+
+    try {
+        list.Insert(7, 50);  // Should throw "Out of range" (correct behavior)
+        list.PrintAll();     // Shouldn't ever get here
+    } catch (std::runtime_error& error) {
+        list.PrintAll();  // Expected: 5 -> 10 -> 20 -> 30 -> 40 -> nullptr
     }
-    std::cout << list.current->value << std::endl;
 
     return 0;
 }
